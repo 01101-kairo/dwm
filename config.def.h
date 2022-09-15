e584414724ea4582eb1224eced6b5a3bf7a70de4
01101-Kairo <kairomilhomem@gmail.com>
2022-09-15 08:38:10 -0300

    update

--- config.def.h
+++ config.def.h
@@ -124,43 +124,41 @@ static Key keys[] = {
         {ALT, XK_equal, spawn, SHCMD("amixer set Capture 5%+;pkill -RTMIN+11 dwmblocks")},
         {ALT, XK_minus, spawn, SHCMD("amixer set Capture 5%-;pkill -RTMIN+11 dwmblocks")},
         {ALT, XK_0, spawn, SHCMD("amixer set Capture toggle;pkill -RTMIN+11 dwmblocks")},
-        {0, XF86XK_AudioStop, spawn,
-            SHCMD("rofi -modi \"clipboard:greenclip print\" -show clipboard "
-                    "-run-command '{cmd}'")},
+        {0, XF86XK_AudioStop, spawn, SHCMD("rofi -modi \"clipboard:greenclip print\" -show clipboard " "-run-command '{cmd}'")},
 
-            /***********************************************************************/
-            /*****************************[atalhos dwm]****************************/
-            /*********************************************************************/
+        /***********************************************************************/
+        /*****************************[atalhos dwm]****************************/
+        /*********************************************************************/
 
-            /*set layouts*/
-            {ALT, XK_t, setlayout, {.v = &layouts[0]}},
-            {ALT, XK_f, setlayout, {.v = &layouts[1]}},
-            {ALT, XK_m, setlayout, {.v = &layouts[2]}},
-            /*atalhos*/
-            {MODKEY, XK_space, setlayout, {0}},
-            {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-            {MODKEY, XK_i, incnmaster, {.i = +1}},
-            {MODKEY | ShiftMask, XK_i, incnmaster, {.i = -1}},
-            {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
-            {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
-            {MODKEY, XK_j, focusstack, {.i = +1}},
-            {MODKEY, XK_k, focusstack, {.i = -1}},
-            {MODKEY, XK_h, setmfact, {.f = -0.05}},
-            {MODKEY, XK_l, setmfact, {.f = +0.05}},
-            {MODKEY, XK_0, view, {.ui = ~0}},
-            {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-            {MODKEY, XK_comma, focusmon, {.i = -1}},
-            {MODKEY, XK_period, focusmon, {.i = +1}},
-            {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-            {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-            {MODKEY | ShiftMask, XK_Return, zoom, {0}},
-            {MODKEY, XK_Tab, view, {0}},
-            {MODKEY, XK_g, togglebar, {0}},
-            {MODKEY, XK_q, killclient, {0}},
-            {MODKEY | ShiftMask, XK_q, quit, {0}},
-            /*etiqueta*/
-            TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4)
-                TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8)
+        /*set layouts*/
+        {ALT, XK_t, setlayout, {.v = &layouts[0]}},
+        {ALT, XK_f, setlayout, {.v = &layouts[1]}},
+        {ALT, XK_m, setlayout, {.v = &layouts[2]}},
+        /*atalhos*/
+        {MODKEY, XK_space, setlayout, {0}},
+        {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+        {MODKEY, XK_i, incnmaster, {.i = +1}},
+        {MODKEY | ShiftMask, XK_i, incnmaster, {.i = -1}},
+        {MODKEY | ShiftMask, XK_j, rotatestack, {.i = +1}},
+        {MODKEY | ShiftMask, XK_k, rotatestack, {.i = -1}},
+        {MODKEY, XK_j, focusstack, {.i = +1}},
+        {MODKEY, XK_k, focusstack, {.i = -1}},
+        {MODKEY, XK_h, setmfact, {.f = -0.05}},
+        {MODKEY, XK_l, setmfact, {.f = +0.05}},
+        {MODKEY, XK_0, view, {.ui = ~0}},
+        {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+        {MODKEY, XK_comma, focusmon, {.i = -1}},
+        {MODKEY, XK_period, focusmon, {.i = +1}},
+        {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
+        {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
+        {MODKEY | ShiftMask, XK_Return, zoom, {0}},
+        {MODKEY, XK_Tab, view, {0}},
+        {MODKEY, XK_g, togglebar, {0}},
+        {MODKEY, XK_q, killclient, {0}},
+        {MODKEY | ShiftMask, XK_q, quit, {0}},
+        /*etiqueta*/
+        TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4)
+            TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8)
 };
 
 /************************************************************************/
