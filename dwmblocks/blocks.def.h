fcac838384275e1a98c3dfbd2fbfd57c88f09a79
01101-Kairo <kairomilhomem@gmail.com>
2022-11-18 16:11:55 -0300

    up up

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -7,12 +7,12 @@ static const Block blocks[] = {
     /* {" ", "date '+%d %b (%a)'", 5, 0}, */
     /* {" ", "date '+%H:%M%p'", 5, 0}, */
     {"", SC("bluetooth"), 0, 9},
-    {" ", SC("audio"), 0, 10},
+    {"", SC("audio"), 0, 10},
     {"", SC("microphone"), 0, 11},
     {" ", SC("network"), 1, 0},
-    {" ", "free -h|awk '/^Mem/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
-    {" ", "free -h|awk '/^Swap/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
-    {" ", SC("cpu"), 1, 0},
+    {" ", "free -h|awk '/^Mem/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
+    {" ", "free -h|awk '/^Swap/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
+    {" ", SC("cpu"), 1, 0},
     {" ", SC("temperature"), 1, 0},
     {" ", "setxkbmap -query|grep -oP us || setxkbmap -query |grep -oP br", 2,
      13},
--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -7,15 +7,15 @@ static const Block blocks[] = {
     /* {" ", "date '+%d %b (%a)'", 5, 0}, */
     /* {" ", "date '+%H:%M%p'", 5, 0}, */
     {"", SC("bluetooth"), 0, 9},
-    {" ", SC("audio"), 0, 10},
+    {"", SC("audio"), 0, 10},
     {"", SC("microphone"), 0, 11},
-    // {" ", "brightnessctl l|grep -oP \"[^\\(]*%\"", 0, 12},
     {" ", SC("network"), 1, 0},
-    {" ", "free -h|awk '/^Mem/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
-    {" ", "free -h|awk '/^Swap/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
-    {" ", SC("cpu"), 1, 0},
+    {" ", "free -h|awk '/^Mem/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
+    {" ", "free -h|awk '/^Swap/ { print $3\"/\"$2 }'|sed s/i//g", 1, 0},
+    {" ", SC("cpu"), 1, 0},
     {" ", SC("temperature"), 1, 0},
-    {" ", "setxkbmap -query|grep -oP us || setxkbmap -query |grep -oP br", 2, 13},
+    {" ", "setxkbmap -query|grep -oP us || setxkbmap -query |grep -oP br", 2,
+     13},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
